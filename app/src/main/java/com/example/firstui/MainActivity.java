01dc3a485d3474b763e19f5a0b0e7f7c8cfbb6c6
karim@gmail.com <karim@gmail.com>
2022-11-12 14:51:25 +0100

    version 1

---
+++ app/src/main/java/com/example/firstui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.firstui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
